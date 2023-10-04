f6692bffb054a1a94df30db97df7a79525c695f9
ltpbje <19836788252@163.com>
2023-10-04 17:41:53 +0800

    03-导航链接传参2-动态路由传参 04-路由重定向 完成

---
+++ 03-导航链接传参2-动态路由传参/router/index.js
@@ -0,0 +1,19 @@
+import Home from '@/views/Home'
+import Search from '@/views/Search'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter) // VueRouter插件初始化
+
+// 创建了一个路由对象
+const router = new VueRouter({
+  // history路由模式
+  mode: 'history',
+  routes: [
+    // 重定向
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: Home },
+    { path: '/search/:word?', component: Search }
+  ]
+})
+
+export default router
